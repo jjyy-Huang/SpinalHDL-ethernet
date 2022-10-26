04b0a0005589349e827288378e3575b43364024e
Jerry Huang <809440459@qq.com>
2022-10-26 19:54:00 +0000

    [Important]more random testcase with testbench

--- src/test/scala/merge/Sim.scala
+++ src/test/scala/merge/Sim.scala
@@ -1,10 +1,9 @@
 package merge
 
-import spinal.core.Component.push
 import spinal.core._
 import spinal.core.sim._
 import spinal.lib._
-import spinal.lib.bus.amba4.axis.Axi4Stream
+import spinal.lib.sim._
 
 import scala.math._
 import scala.util.Random
@@ -44,76 +43,173 @@ object Sim extends App {
 		.compile(new AXI4StreamInsertTopLevel(config))
 		.doSim { dut =>
 			dut.clockDomain.forkStimulus(period = 10)
-			dut.io.dataOut_AXIS.port.ready #= true
+			SimTimeout(10000)
 			def driverInit(): Unit ={
-				dut.io.headerIn_AXIS.port.valid #= false
-				dut.io.headerIn_AXIS.port.keep #= 0
 				dut.io.headerIn_AXIS.port.user #= 0
+				dut.io.headerIn_AXIS.port.keep #= 0
 				dut.io.headerIn_AXIS.port.data #= 0
+				dut.io.headerIn_AXIS.port.valid #= false
 
-				dut.io.dataIn_AXIS.port.valid #= false
 				dut.io.dataIn_AXIS.port.keep #= 0
 				dut.io.dataIn_AXIS.port.data #= 0
+				dut.io.dataIn_AXIS.port.valid #= false
 				dut.io.dataIn_AXIS.port.last #= false
 
+				dut.io.dataOut_AXIS.port.ready #= true
 			}
+
+			val scoreboard = ScoreboardInOrder[Int]()
+
+			def SumUpPow(a: Int): Int = {
+				var sum: Int = 0
+				for (j <- config.DATA_BYTE_WD - a until config.DATA_BYTE_WD) {
+					sum += pow(2, j).toInt
+				}
+				sum
+			}
+
+			def SumDownPow(a: Int): Int = {
+				var sum: Int = 0
+				for (j <- 0 until a) {
+					sum += pow(2, j).toInt
+				}
+				sum
+			}
+
+			def DataDrive(): Unit ={
+				fork {
+					def issueSeq(t: Int): Unit = {
+						var times = 0
+						while (times < t) {
+							if (dut.io.dataIn_AXIS.port.ready.toBoolean) {
+								dut.io.dataIn_AXIS.port.data.randomize()
+								times += 1
+							}
+							dut.io.dataIn_AXIS.port.valid #= true
+							dut.io.dataIn_AXIS.port.keep #= pow(2, config.DATA_BYTE_WD).toInt - 1
+							dut.io.dataIn_AXIS.port.last #= false
+							dut.clockDomain.waitRisingEdge()
+						}
+						dut.io.dataIn_AXIS.port.data.randomize()
+						dut.io.dataIn_AXIS.port.valid #= true
+						dut.io.dataIn_AXIS.port.last #= true
+						dut.io.dataIn_AXIS.port.keep #= SumUpPow(Random.nextInt(config.DATA_BYTE_WD) + 1)
+						dut.clockDomain.onNextSampling {
+							dut.io.dataIn_AXIS.port.data #= 0
+							dut.io.dataIn_AXIS.port.valid #= false
+							dut.io.dataIn_AXIS.port.last #= false
+							dut.io.dataIn_AXIS.port.keep #= pow(2, config.DATA_BYTE_WD).toInt - 1
+						}
+						dut.clockDomain.waitRisingEdge(20)
+					}
+
+					while (true) {
+
+						if (dut.getHeader.toBoolean) {
+							issueSeq(Random.nextInt(16) + 1)
+						}
+
+						dut.clockDomain.waitRisingEdge()
+					}
+				}
+			}
+
+			def HeaderDrive(): Unit = {
+				fork {
+					def SetHeader(): Unit = {
+						var t = Random.nextInt(config.DATA_BYTE_WD + 1)
+						dut.io.headerIn_AXIS.port.data.randomize()
+						dut.io.headerIn_AXIS.port.valid #= true
+						dut.io.headerIn_AXIS.port.keep #= SumDownPow(t)
+						dut.io.headerIn_AXIS.port.user #= t
+					}
+
+					def ResetHeader(): Unit = {
+						dut.io.headerIn_AXIS.port.data #= 0
+						dut.io.headerIn_AXIS.port.valid #= false
+						dut.io.headerIn_AXIS.port.keep #= 0
+						dut.io.headerIn_AXIS.port.user #= 0
+					}
+
+					while (true) {
+						if (!dut.getHeader.toBoolean) {
+							SetHeader()
+							dut.clockDomain.waitRisingEdge()
+							ResetHeader()
+						}
+						dut.clockDomain.waitRisingEdge()
+					}
+				}
+			}
+
+			def RandomReady(): Unit ={
+				fork {
+					while (true) {
+						if (Random.nextFloat() < 0.1f) {
+							dut.io.dataOut_AXIS.port.ready #= false
+							dut.clockDomain.waitRisingEdge(Random.nextInt(4))
+							dut.io.dataOut_AXIS.port.ready #= true
+						}
+						dut.clockDomain.waitRisingEdge()
+					}
+				}
+			}
+
+			def Monitor(): Unit ={
+				fork {
+					while (true) {
+						if (!dut.getHeader.toBoolean) {
+							if (dut.io.headerIn_AXIS.port.valid.toBoolean && dut.io.headerIn_AXIS.port.ready.toBoolean) {
+								var data : BigInt = dut.io.headerIn_AXIS.port.data.toBigInt
+								var keep: BigInt = dut.io.headerIn_AXIS.port.keep.toBigInt
+//								println(data.toString(16) + "  --  " + keep.toString(2))
+								for (i <- config.DATA_BYTE_WD-1 downto 0) {
+									if ((pow(2, i).toInt & keep) >> i == 1) {
+//										println((((0xff << i * 8) & data) >> i * 8).toString(16))
+										scoreboard.pushRef((((0xff << i * 8) & data) >> i * 8).toInt)
+									}
+								}
+							}
+						} else {
+							if (dut.io.dataIn_AXIS.port.valid.toBoolean && dut.io.dataIn_AXIS.port.ready.toBoolean) {
+								var data: BigInt = dut.io.dataIn_AXIS.port.data.toBigInt
+								var keep: BigInt = dut.io.dataIn_AXIS.port.keep.toBigInt
+//								println(data.toString(16) + "  --  " + keep.toString(2))
+								for (i <- config.DATA_BYTE_WD - 1 downto 0) {
+									if ((pow(2, i).toInt & keep) >> i == 1) {
+//										println((((0xff << i * 8) & data) >> i * 8).toString(16))
+										scoreboard.pushRef((((0xff << i * 8) & data) >> i * 8).toInt)
+									}
+								}
+							}
+						}
+
+						if (dut.io.dataOut_AXIS.port.valid.toBoolean && dut.io.dataOut_AXIS.port.ready.toBoolean) {
+							var data: BigInt = dut.io.dataOut_AXIS.port.data.toBigInt
+							var keep: BigInt = dut.io.dataOut_AXIS.port.keep.toBigInt
+//							println(data.toString(16) + "  --  " + keep.toString(2))
+							for (i <- config.DATA_BYTE_WD - 1 downto 0) {
+								if ((pow(2, i).toInt & keep) >> i == 1) {
+//									println((((0xff << i * 8) & data) >> i * 8).toString(16))
+									scoreboard.pushDut((((0xff << i * 8) & data) >> i * 8).toInt)
+								}
+							}
+						}
+
+						dut.clockDomain.waitRisingEdge()
+					}
+				}
+			}
+
 			driverInit()
-
-			for (i <- 0 until 10) {
-				dut.clockDomain.waitRisingEdge()
-			}
-			dut.io.headerIn_AXIS.port.valid #= true
-			dut.io.headerIn_AXIS.port.keep #= 15
-			dut.io.headerIn_AXIS.port.user #= 4
-			dut.io.headerIn_AXIS.port.data #= abs(Random.nextInt() % pow(2, 32)).toInt
-			dut.clockDomain.waitRisingEdge()
-
-			dut.io.headerIn_AXIS.port.valid #= false
-			dut.io.headerIn_AXIS.port.keep #= 0
-			dut.io.headerIn_AXIS.port.user #= 0
-			dut.io.headerIn_AXIS.port.data #= 0
-			dut.clockDomain.waitRisingEdge(3)
-
-			var times: Int = 4
-			for (i <- 0 until times) {
-				dut.io.dataIn_AXIS.port.valid #= true
-				dut.io.dataIn_AXIS.port.keep #= 15
-				dut.io.dataIn_AXIS.port.data #= abs(Random.nextInt() % pow(2, 32)).toInt
-				dut.io.dataIn_AXIS.port.last #= false
-				dut.clockDomain.waitRisingEdge()
-			}
-
-			dut.io.dataIn_AXIS.port.valid #= false
-			dut.io.dataOut_AXIS.port.ready #= false
-			dut.clockDomain.waitRisingEdge(2)
-			dut.io.dataOut_AXIS.port.ready #= true
-			dut.clockDomain.waitRisingEdge(2)
+			dut.clockDomain.waitRisingEdge(50)
+			DataDrive()
+			HeaderDrive()
+			RandomReady()
+			Monitor()
 
 
-			for (i <- 0 until 2) {
-				dut.io.dataIn_AXIS.port.valid #= true
-				dut.io.dataIn_AXIS.port.keep #= 15
-				dut.io.dataIn_AXIS.port.data #= abs(Random.nextInt() % pow(2, 32)).toInt
-				dut.io.dataIn_AXIS.port.last #= false
-				dut.clockDomain.waitRisingEdge()
-			}
-
-			dut.io.dataIn_AXIS.port.valid #= true
-			dut.io.dataIn_AXIS.port.keep #= 12
-			dut.io.dataIn_AXIS.port.data #= abs(Random.nextInt() % pow(2, 32)).toInt
-			dut.io.dataIn_AXIS.port.last #= true
-			dut.clockDomain.waitRisingEdge()
-
-
-			for (i <- 0 until times) {
-				dut.io.dataIn_AXIS.port.valid #= false
-				dut.io.dataIn_AXIS.port.keep #= 0
-				dut.io.dataIn_AXIS.port.data #= 0
-				dut.io.dataIn_AXIS.port.last #= false
-				dut.clockDomain.waitRisingEdge()
-			}
-
-//				spinal.lib.sim.StreamDriver
+			dut.clockDomain.waitActiveEdgeWhere(scoreboard.matches == 100)
 
 			//	      simSuccess()
 			}
